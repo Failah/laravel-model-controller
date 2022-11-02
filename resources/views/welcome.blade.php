beaa7747c7d51e1071a947e8a09204a06dde7281
ValerioScharizza <valerio.schiarizza@live.it>
2022-11-02 18:25:36 +0100

    finish

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Test</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Laravel-Model-Controller</title>
 </head>
 
 <body>
-    Test
+    <h1 class="container">Laravel-Model-Controller Test Exercise</h1>
+    <div class="container">
+        @foreach ($movies as $movie)
+            <div class="card">
+                <h2>Title: <span>{{ $movie['title'] }}</span></h2>
+                <div>Release Date: <span>{{ $movie['date'] }}</span></div>
+                <p>Nationality: <span>{{ $movie['nationality'] }}</span></p>
+                <h4>Average Vote: <span>{{ $movie['vote'] }}</span></h4>
+            </div>
+        @endforeach
+    </div>
 </body>
 
 </html>
